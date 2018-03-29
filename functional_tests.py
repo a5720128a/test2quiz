2dee7f0b0059d6060db249dc3c86987447bad6df
suppakan <suppakan@ubuntu.ubuntu-domain>
2018-03-29 13:44:53 +0700

    fist simple homepage

--- functional_tests.py
+++ functional_tests.py
@@ -14,13 +14,14 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_quiz_and_use_functions(self):
         
         # Kan has heard about a cool new online "quiz" app.
-        self.browser.get('http://localhost:8000/quiz/')
+        self.browser.get('http://localhost:8000/')
         # Kan goes to check out its homepage.
-        
         # Kan notices the page title and header mention quiz.
-        
+        self.assertIn('quiz', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('quiz', header_text)
         # Kan enter the first quiz in webapp.
-        
+        self.fail('Finish the test!')
         # Kan choose first checkblock as true.
 
         # Kan choose 2 other checkblock as false.
